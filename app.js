963600fb1c1cc8fe85086fdb91e516f51c7f0f8c
Benjamin Dreux <benjamin.dreux@code3.ca>
2014-02-11 22:14:44 -0500

    add content type header

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 http = require('http');
 http.createServer(function(req,res){
-  res.writeHead(200, {});
+  res.writeHead(200, {'content-type':'application/json'});
   res.write("{alive:true}");
   res.end();
 
