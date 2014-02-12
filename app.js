a4fb16bd3e3ed0bd3f52d84fac66ccb1f335558b
Benjamin Dreux <benjamin.dreux@code3.ca>
2014-02-12 08:19:11 -0500

    oops miss the header

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 http = require('http');
 http.createServer(function(req,res){
+  // res.writeHead(200, {'content-type':'application/json'});
   res.end("{\"alive\":true}");
+
 }).listen(process.env.PORT || 5000);
