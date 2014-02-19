c7b873566222dae343bca4273b24d3150d520a5d
Benjamin Dreux <benjamin.dreux@code3.ca>
2014-02-18 22:35:44 -0500

    simple web page

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 http = require('http');
 http.createServer(function(req,res){
-  res.writeHead(200, {'content-type':'application/json'});
-  res.end("{\"alive\":true}");
+
+  res.writeHead(200, {'content-type':'text/html'});
+  res.end("<html><body>Hello Yose</body></html>")
+  // res.writeHead(200, {'content-type':'application/json'});
+  // res.end("{\"alive\":true}");
 
 }).listen(process.env.PORT || 5000);
