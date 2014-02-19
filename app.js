20cb0e82fea43826217e8bfb61eefed1e6f38ce7
Benjamin Dreux <benjamin.dreux@code3.ca>
2014-02-18 22:45:30 -0500

    enable lvl1 and lvl2

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 http = require('http');
 http.createServer(function(req,res){
 
-  res.writeHead(200, {'content-type':'text/html'});
-  res.end("<html><body>Hello Yose</body></html>")
-  // res.writeHead(200, {'content-type':'application/json'});
-  // res.end("{\"alive\":true}");
+  if(req.url == '/'){
+    res.writeHead(200, {'content-type':'text/html'});
+    res.end("<html><body>Hello Yose</body></html>");
+  }
+
+  if(req.url == '/ping'){
+    res.writeHead(200, {'content-type':'application/json'});
+    res.end("{\"alive\":true}");
+  }
 
 }).listen(process.env.PORT || 5000);
