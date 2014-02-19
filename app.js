14e50afc0b5f5d56b449fd46cb0188231911badc
Benjamin Dreux <benjamin.dreux@code3.ca>
2014-02-18 22:59:13 -0500

    level 3 correction class -> id

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ http = require('http');
 http.createServer(function(req,res){
 
   if(req.url == '/'){
+    var html = ""+
+      "<html>"+
+        "<body>"+
+          "Hello Yose"+
+          "<a id='repository-link' href='https://github.com/benzen/yose-benzen'>"+
+            "A link to my party"+
+          "</a>"+
+        "</body>"+
+      "</html>";
     res.writeHead(200, {'content-type':'text/html'});
-    res.end("<html><body>Hello Yose</body></html>");
+
+    res.end(html);
   }
 
   if(req.url == '/ping'){
